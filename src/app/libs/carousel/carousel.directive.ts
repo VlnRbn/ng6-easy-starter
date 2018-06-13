1af78013b363965b319466e750ebe875cf2029cb
Velen Aranha <ruben.v@pacewisdom.com>
2018-06-14 01:06:37 +0530

    switchMap() in carousel directive

--- src/app/libs/carousel/carousel.directive.ts
+++ src/app/libs/carousel/carousel.directive.ts
@@ -1,66 +1,73 @@
 import { Directive, ViewContainerRef, TemplateRef, Input, OnInit } from '@angular/core';
 
 
-import { interval } from 'rxjs';
-import { take, timeInterval } from 'rxjs/operators'
+import { interval, Observable, from, Subject, BehaviorSubject } from 'rxjs';
+import { take, timeInterval, switchMap, repeat, } from 'rxjs/operators'
 
 export interface CarouselContext {
-    $implicit : string,
-    controller : {
-        next : () => void;
-        prev : () => void;
+    $implicit: string,
+    controller: {
+        next: () => void;
+        prev: () => void;
     }
 }
 
 @Directive({ selector: '[carousel]' })
 export class CarouselDirective implements OnInit {
-    constructor(
-        private viewContainerRef : ViewContainerRef,
-        private temRef : TemplateRef<CarouselContext>
-    ) { }
 
-    @Input('carouselFrom') images : string[];
+    private imageChanged$ = new BehaviorSubject<void>(null);
+
+    @Input('carouselFrom') images: string[];
 
     context: CarouselContext | null = null;
     index: number = 0;
 
-    private repeat$  = interval(8000 /* ms */)
-    .pipe(
-        timeInterval(),
-    )
+    constructor(
+        private viewContainerRef: ViewContainerRef,
+        private temRef: TemplateRef<CarouselContext>
+    ) { }
 
-    ngOnInit(){
+    ngOnInit() {
         this.context = {
             $implicit: this.images[0],
             controller: {
                 next: () => this.next(),
                 prev: () => this.prev()
-            }
-        }; 
+            },
+        };
 
         this.viewContainerRef.createEmbeddedView(this.temRef, this.context);
 
-
-        this.repeat$.subscribe(
-            ()=>{ 
-                this.next() 
-            }
+        this.imageChanged$.pipe(
+            switchMap(
+                () => interval(1000 /* ms */)
+            )
         )
+        .subscribe(
+            (num) => {
+                if (num == 5){
+                    this.next()
+                }
+            }
+        );
+    
     }
 
-    next(): void{
+    next(): void {
         this.index++;
         if (this.index >= this.images.length) {
             this.index = 0;
         }
-        this.context.$implicit = this.images[this.index]
+        this.context.$implicit = this.images[this.index];
+        this.imageChanged$.next(null);
     }
 
-    prev(): void{
+    prev(): void {
         this.index--;
-        if (this.index < 0 ) {
+        if (this.index < 0) {
             this.index = this.images.length - 1;
         }
-        this.context.$implicit = this.images[this.index]
+        this.context.$implicit = this.images[this.index];
+        this.imageChanged$.next(null);
     }
 }
